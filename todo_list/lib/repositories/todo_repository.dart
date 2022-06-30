6fe613a844de4b41f5b203949319a851f0cf34a6
Gustavo Santos <gustavo.santos@keyrus.com.br>
2022-06-30 13:31:10 -0300

    reading todo list

--- todo_list/lib/repositories/todo_repository.dart
+++ todo_list/lib/repositories/todo_repository.dart
@@ -4,15 +4,20 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/todo.dart';
 
-class TodoRepository {
-  TodoRepository() {
-    SharedPreferences.getInstance().then((value) => sharedPreferences = value);
-  }
+const todoListKey = 'todo_list';
 
+class TodoRepository {
   late SharedPreferences sharedPreferences;
+
+  Future<List<Todo>> getTodoList() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    final String jsonString = sharedPreferences.getString(todoListKey) ?? '[]';
+    final List jsonDecoded = json.decode(jsonString) as List;
+    return jsonDecoded.map((e) => Todo.fromJson(e)).toList();
+  }
 
   void saveTodoList(List<Todo> todos) {
     final jsonString = json.encode(todos);
-    print(jsonString);
+    sharedPreferences.setString(todoListKey, jsonString);
   }
 }
