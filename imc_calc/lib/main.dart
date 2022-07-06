78a4093f154e181c5c6cef76982ea15ab0f18760
Gustavo Santos <gustavo.santos@keyrus.com.br>
2022-07-06 10:36:56 -0300

    Imc calculator

--- imc_calc/lib/main.dart
+++ imc_calc/lib/main.dart
@@ -14,18 +14,54 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  String _infoText = "Informe seus dados!";
+
+  void _resetField() {
+    weightController.text = "";
+    heightController.text = "";
+    setState(() {
+      _infoText = "Informe seus dados!";
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do Peso ${imc.toStringAsPrecision(3)}.";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso ideal ${imc.toStringAsPrecision(3)}.";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente acima do peso ${imc.toStringAsPrecision(3)}.";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade grau I ${imc.toStringAsPrecision(3)}.";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade grau II ${imc.toStringAsPrecision(3)}.";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade grau III ${imc.toStringAsPrecision(3)}.";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Calculadora IMC"),
+        title: const Text("Calculadora IMC"),
         centerTitle: true,
         backgroundColor: Colors.green,
-        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.refresh))],
+        actions: [
+          IconButton(onPressed: _resetField, icon: const Icon(Icons.refresh))
+        ],
       ),
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
@@ -34,21 +70,23 @@ class _HomeState extends State<Home> {
               size: 120,
               color: Colors.green,
             ),
-            const TextField(
+            TextField(
+              controller: weightController,
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                   labelText: "Peso (kg)",
                   labelStyle: TextStyle(color: Colors.green)),
               textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.green, fontSize: 25),
+              style: const TextStyle(color: Colors.green, fontSize: 25),
             ),
-            const TextField(
+            TextField(
+              controller: heightController,
               keyboardType: TextInputType.number,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                   labelText: "Altura (cm)",
                   labelStyle: TextStyle(color: Colors.green)),
               textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.green, fontSize: 25),
+              style: const TextStyle(color: Colors.green, fontSize: 25),
             ),
             Padding(
               padding: const EdgeInsets.only(top: 10, bottom: 10),
@@ -57,17 +95,15 @@ class _HomeState extends State<Home> {
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
                       primary: Colors.green,
-                      textStyle: TextStyle(fontSize: 25)),
-                  onPressed: () {
-                    print("");
-                  },
-                  child: Text("Calcular"),
+                      textStyle: const TextStyle(fontSize: 25)),
+                  onPressed: _calculate,
+                  child: const Text("Calcular"),
                 ),
               ),
             ),
             Text(
-              "Info",
-              style: TextStyle(fontSize: 25, color: Colors.green),
+              _infoText,
+              style: const TextStyle(fontSize: 25, color: Colors.green),
               textAlign: TextAlign.center,
             )
           ],
