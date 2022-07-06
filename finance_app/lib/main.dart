1d9db12bf460fa51bf250236709aa1abdcdb717d
Gustavo Santos <gustavo.santos@keyrus.com.br>
2022-07-06 13:06:15 -0300

    Future widget

---
+++ finance_app/lib/main.dart
@@ -0,0 +1,69 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const request = "https://api.hgbrasil.com/finance/quotations?key=45a9b634";
+
+void main() async {
+  print(await getData());
+
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return const Center(
+                child: Text(
+                  "Carregando Dados...",
+                  style: TextStyle(color: Colors.amber, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            default:
+              if (snapshot.hasError) {
+                return const Center(
+                  child: Text(
+                    "Erro ao carregar dados",
+                    style: TextStyle(color: Colors.amber, fontSize: 25),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              } else {
+                return Container(color: Colors.green);
+              }
+          }
+        },
+      ),
+    );
+  }
+}
